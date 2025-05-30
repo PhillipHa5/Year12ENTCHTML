81e2f85842337c2559a39d32ca75ebdc0afb9610
PhillipHa5 <phillip.ha5@education.nsw.gov.au>
2025-05-31 00:29:18 +1000

    V1
    Created Navbar and started on home page

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let popoutbtn = document.querySelector('#menubtn');
+let sidebar = document.querySelector(".sidebar");
+let pagecontents = document.querySelector(".PageContents")
+
+popoutbtn.onclick = function() {
+    sidebar.classList.toggle("active");
+    pagecontents.classList.toggle("active");
+}
