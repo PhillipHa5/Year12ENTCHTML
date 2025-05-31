f872eb723de8b7b9da7109ada9c932b9b720b1a7
PhillipHa5 <phillip.ha5@education.nsw.gov.au>
2025-05-31 18:42:06 +1000

    V2
    Created table for data

--- scripts.js
+++ scripts.js
@@ -6,3 +6,18 @@ popoutbtn.onclick = function() {
     sidebar.classList.toggle("active");
     pagecontents.classList.toggle("active");
 }
+
+new DataTable('#MainTable', {
+    ajax: 'https://script.google.com/macros/s/AKfycbyYpVWPHCnvd__x1XrPVJfLkxylslBA7b2Svrr13ZMxA-iJpJ5Lc1mWkioB7XUCwhpJ/exec',
+    columns: [
+        { data: 'count'},
+        { data: 'user_id' },
+        { data: 'book_id' },
+        { data: 'borrow_date' },
+        { data: 'return_date' },
+        { data: 'reservation_status' },
+        { data: 'overdue_status' },
+        { data: 'user_role'},
+        { data: 'book_category'},
+    ]
+});
